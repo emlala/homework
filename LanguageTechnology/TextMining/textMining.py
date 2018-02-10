a8b46b51288249e15ec3715d02d6360f24567628
Emmi Lahtisalo <32639479+emlala@users.noreply.github.com>
2018-02-10 13:42:02 +0200

    Update textMining.py

--- LanguageTechnology/TextMining/textMining.py
+++ LanguageTechnology/TextMining/textMining.py
@@ -1,44 +1,45 @@
-#Emmi Lahtisalo, 014163276
+#Emmi Lahtisalo
 
-'''Assignment 3.3'''
+'''Mining Weather Forecasts'''
 
 ##########################
 
 from urllib import request
 from bs4 import BeautifulSoup
 
+# Choosing Helsinki area
 url = "http://www.foreca.fi/Finland/Helsinki"
 html = request.urlopen(url).read()
 soup = BeautifulSoup(html, "lxml")
 
-#Otsikko
+# Title
 title = soup.find("title").get_text(" ", strip=True) 
 
-#Havaintoaika
+# Forecast time
 time = soup.find("div", attrs={"style": "float: left; width: 95%;"}).strong.get_text(" ", strip=True)
 
-#Tämän hetken lämpötila
+# Current temperature
 currentCelsius = soup.find("span", attrs={"class": "cold txt-xxlarge"}).get_text(" ", strip=True)
 
-#Tämän hetken tarkemmat säätiedot
+# Current weather conditions
 currentWeather = soup.find("div", attrs={"class": "right txt-tight"}).get_text("\n", strip=True)
 
-#Kolmen päivän ennuste
+# Three day forecast
 ThreeDayForecast = soup.find_all("div", attrs={"class": "c2_a"})
 
-#Kuumimmat ja kylmimmät mitatut lämpötilat maapallolla
+# Hottest and coldest temperatures on Earth
 hottest = soup.find_all("div", attrs={"class": "triviarow"})[0].text.strip()
 coldest = soup.find_all("div", attrs={"class": "triviarow"})[3].text.strip()
     
 
-#Tulosteet haetuille tiedoille
+# Prints
 
-print(title + "\nHavaintoaika: " + time + "\nLämpötila: " + currentCelsius + "\n\n" + currentWeather)
+print(title + "\nForecast time: " + time 
+      + "\nTemperature: " + currentCelsius + "\n\n" + currentWeather)
 
-print("\nKuumin mitattu lämpötila maailmalla: " + ' '.join(hottest.split()) + "\nKylmin mitatttu lämpötila maailmalla: " + ' '.join(coldest.split()) + "\n\nKolmen päivän ennuste:")
+print("\nHottest place on Earth: " + ' '.join(hottest.split()) 
+      + "\nColdest place on Earth: " + ' '.join(coldest.split()) 
+      + "\n\nThree Day Forecast:")
 
 for div in ThreeDayForecast:
     print("\n" + div.get_text("\n", strip=True))
-
-
-#Toivottavasti ääkköset ei aiheuta mitään ongelmia.
