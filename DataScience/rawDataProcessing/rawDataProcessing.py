e2ed3a7f8476903a50f7b31090426f224f0621d2
Emmi Lahtisalo <32639479+emlala@users.noreply.github.com>
2018-02-10 13:43:10 +0200

    Update rawDataProcessing.py

--- DataScience/rawDataProcessing/rawDataProcessing.py
+++ DataScience/rawDataProcessing/rawDataProcessing.py
@@ -1,23 +1,18 @@
 # Emmi Lahtisalo
-# 014163276
 
-'''Exercise 1.2'''
+'''Raw Data Processing'''
 
 import csv, json
 import string
 import pandas as pd
 import nltk
 
-# 3. a)
-
 df = pd.read_json("Automotive_5.json", lines=True)
 
-# 3. b) lowercasing the texts in reviewText column
-
+# lowercasing the texts in reviewText column
 df['reviewText'] = df['reviewText'].str.lower()
 
-# 3. c) removing punctuation...
-
+# removing punctuation...
 def del_punct(x):
     try:
         x = ''.join(ch for ch in x if ch not in set(string.punctuation))
@@ -27,19 +22,18 @@ def del_punct(x):
 df['reviewText'] = df['reviewText'].apply(del_punct)
 
 # ... and stopwords
-
 from nltk.corpus import stopwords
 stopwords = stopwords.words('english')
 df['reviewText'] = df['reviewText'].str.split()
 df['reviewText'] = df['reviewText'].apply(lambda x: [item for item in x if item not in stopwords])
 
-# 3. d) stemming
+# stemming
 from nltk.stem.snowball import SnowballStemmer
 stemmer = SnowballStemmer('english')
 
 df['reviewText'] = df["reviewText"].apply(lambda x: [stemmer.stem(y) for y in x])
 
-# 3. e) filtering data
+# filtering data
 pos = df.loc[df['overall'] == 4, 'reviewText']
 fives = df.loc[df['overall'] == 5, 'reviewText']
 
